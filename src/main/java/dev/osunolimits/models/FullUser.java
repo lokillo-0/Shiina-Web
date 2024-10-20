8b03c3c3c7e247a6fd4a6d37bb1c8253df1ceaa0
marcandreher <inbox@osu-server-list.com>
2024-10-20 12:34:21 +0200

    add: first user page

--- src/main/java/dev/osunolimits/models/FullUser.java
+++ src/main/java/dev/osunolimits/models/FullUser.java
@@ -26,36 +26,36 @@ public class FullUser {
         private int plays;
         private int playtime;
         private int acc;
-        private int maxCombo;
-        private int totalHits;
-        private int replayViews;
-        private int xhCount;
-        private int xCount;
-        private int shCount;
-        private int sCount;
-        private int aCount;
+        private int max_combo;
+        private long total_hits;
+        private long replay_views;
+        private int xh_count;
+        private int x_count;
+        private int sh_count;
+        private int s_count;
+        private int a_count;
         private int rank;
-        private int countryRank;
+        private int country_rank;
     }
 
     @Data
     public class UserInfo {
         private int id;
         private String name;
-        private String safeName;
+        private String safe_name;
         private int priv;
         private String country;
-        private int silenceEnd;
-        private int donorEnd;
-        private int creationTime;
-        private int latestActivity;
-        private int clanId;
-        private int clanPriv;
-        private int preferredMode;
-        private int playStyle;
-        private String customBadgeName;
-        private String customBadgeIcon;
-        private String userpageContent;
+        private int silence_end;
+        private int donor_end;
+        private int creation_time;
+        private int latest_activity;
+        private int clan_id;
+        private int clan_priv;
+        private int preferred_mode;
+        private int play_style;
+        private String custom_badge_name;
+        private String custom_badge_icon;
+        private String userpage_content;
     }
 
 }
