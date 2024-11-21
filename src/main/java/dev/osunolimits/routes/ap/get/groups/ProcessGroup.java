d4ba761596550a442203be51b49216e49bca1531
marcandreher <inbox@osu-server-list.com>
2024-11-21 08:49:17 +0100

    ref: oops I forgot

--- src/main/java/dev/osunolimits/routes/ap/get/groups/ProcessGroup.java
+++ src/main/java/dev/osunolimits/routes/ap/get/groups/ProcessGroup.java
@@ -13,6 +13,7 @@ import dev.osunolimits.modules.GroupRegistry;
 import dev.osunolimits.modules.Shiina;
 import dev.osunolimits.modules.ShiinaRoute;
 import dev.osunolimits.modules.ShiinaRoute.ShiinaRequest;
+import dev.osunolimits.utils.osu.PermissionHelper;
 import spark.Request;
 import spark.Response;
 
@@ -28,6 +29,16 @@ public class ProcessGroup extends Shiina {
     @Override
     public Object handle(Request req, Response res) throws Exception {
         ShiinaRequest shiina = new ShiinaRoute().handle(req, res);
+
+        if(!shiina.loggedIn) {
+            res.redirect("/login");
+            return notFound(res, shiina);
+        }
+
+        if(!PermissionHelper.hasPrivileges(shiina.user.priv, PermissionHelper.Privileges.ADMINISTRATOR)) {
+            res.redirect("/");
+            return notFound(res, shiina);
+        }
 
         String id = req.queryParams("id");
         if(id == null) {
