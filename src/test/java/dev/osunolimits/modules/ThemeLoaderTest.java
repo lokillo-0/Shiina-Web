63d61f5c331565200ff8edb09a3c3b6ff25a3ee1
marcandreher <inbox@osu-server-list.com>
2024-12-20 16:38:20 +0100

    fix: unit test crashing

--- src/test/java/dev/osunolimits/modules/ThemeLoaderTest.java
+++ src/test/java/dev/osunolimits/modules/ThemeLoaderTest.java
@@ -5,19 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.HashMap;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import dev.osunolimits.main.App;
-import dev.osunolimits.main.Init;
 import dev.osunolimits.modules.utils.ThemeLoader;
 
 class ThemeLoaderTest {
 
     @BeforeEach
     void setUp() throws Exception {
-        Init init = new Init();
-        App.customization = init.initializeCustomizations();
+        App.customization = new HashMap<String, Object>();
+        App.customization.put("theme", "modern");
     }
 
     @Test
@@ -36,7 +37,7 @@ class ThemeLoaderTest {
 
     @Test
     void testCurrentThemeSelection() {
-        App.customization.put("theme", "modern");
+        
         ThemeLoader.loadThemes();
         assertNotNull(ThemeLoader.currentTheme, "Current theme should be set.");
         assertEquals("modern", ThemeLoader.currentTheme.getName(), "Expected current theme to be 'modern'.");
